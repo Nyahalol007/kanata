22497cbf427219d89c5c41523a4672c16fab2745
Pavaresorn Malai <61514399+Varkaria@users.noreply.github.com>
2021-07-19 17:45:52 +0700

    [+] fixing warp avalible length & if couldn't found warp

--- main.py
+++ main.py
@@ -14,18 +14,26 @@ log('Connecting to Discord server', Ansi.GRAY)
 async def on_message(message):
     if message.content in ['วาป', 'ขอวาปหน่อย', 'อยากได้วาป', 'วาปหน่อย']:
         if message.attachments:
-            log("There is an attachment", Ansi.BLUE)
             f = await message.channel.send(f'{message.author.mention} กำลังหาให้อยู่')
             res = sauce.from_url(message.attachments[0].url)
             await f.delete()
+            e = []
 
-            embed=discord.Embed(title=f"ผลจากการค้นหา | มี {len(res)} วาป", color=0xa5fe9f)
+            for i in res:
+                if i.urls and i.similarity > 70:
+                    e.append(i)
+
+            embed = discord.Embed(title=f"ผลจากการค้นหา | มี {len(e)} วาป", color=0xa5fe9f)
             embed.set_author(name=f"วาปของ {message.author}", icon_url=message.author.avatar_url)
             embed.set_thumbnail(url="https://i.imgur.com/hL5poNS.gif")
             for i in res:
-                if i.urls:
+                if i.urls and i.similarity > 70:
                     embed.add_field(name=f"[{i.author}] {i.title} | ✅ {i.similarity}%", value=f"{i.urls[0]}", inline=False)
             embed.set_footer(text="ได้แล้วก็เก็บไว้ด้วยไอ่เหี้ย เสียทรัพยากรเซิฟกูไอ่สัส")
+            
+            if not e:
+                return await message.channel.send(f"{message.author.mention} หาวาปไม่เจอ โชคร้ายหน่อยนะไอ่โง่ หาเอาเอง 555555555555555555555")
+            
             return await message.channel.send(embed=embed)
 
 @bot.event
